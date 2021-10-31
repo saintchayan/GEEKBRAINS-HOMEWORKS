2c2c4117b9e4ea5549a56367e5c6273f676ad57e
saintchayan <ya.giggs2013@yandex.ru>
2021-10-31 18:47:54 +0300

    update

--- lesson2task1.py
+++ lesson2task1.py
@@ -1,6 +1,5 @@
 # task1
-
-list1 = [True, 1, 2.0, '3']
-type(list)
+a = list(input())
+print(type(a))
 
 
--- lesson2task2.py
+++ lesson2task2.py
@@ -1,8 +1,8 @@
 # task2
 print('Заполните список необходимыми элементами:')
-a = [int(x) for x in input().split()]
+a = [x for x in input()]
 for i in range(0, len(a)-1, 2):
     a[i], a[i + 1] = a[i + 1], a[i]
-print(" ".join(str(i) for i in a))
+print(" ".join([str(i) for i in a]))
 
 
