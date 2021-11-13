d9acdb9622309e00a49f57200a5a0bac6a4073da
saintchayan <ya.giggs2013@yandex.ru>
2021-11-13 16:34:13 +0300

    lesson4

--- lesson4task2.py
+++ lesson4task2.py
@@ -6,6 +6,6 @@
 Результат: [12, 44, 4, 10, 78, 123].
 """
 list1 = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-list2 = [list1[i] for i in range(len(list1)) if list1[i-1] < list1[i]]
+list2 = [list1[i] for i in range(1, len(list1)) if list1[i-1] < list1[i]]
 print(list1)
 print(list2)
